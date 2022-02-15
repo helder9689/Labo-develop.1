0f0b8b2e375970b07abe51aa926bc04da4d703a3
Helder Reis <20009@ecam.be>
2022-02-15 14:42:59 +0100

    Add fact()

---
+++ utils.py
@@ -0,0 +1,44 @@
+def fact(n):
+	"""Computes the factorial of a natural number.
+	
+	Pre: -
+	Post: Returns the factorial of 'n'.
+	Throws: ValueError if n < 0
+	"""
+	if n>=0:
+		result=1
+		for elem in range(n):
+			result=result*(n-elem)
+		return result
+	else:
+		raise ValueError()
+print(fact(-1))
+
+def roots(a, b, c):
+	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
+	
+	Pre: -
+	Post: Returns a tuple with zero, one or two elements corresponding
+		to the roots of the ax^2 + bx + c polynomial.
+	"""          #doc string *** *** permet de faire la documentation de la fonction et sur pls lignes
+	
+
+def integrate(function, lower, upper):
+	"""Approximates the integral of a fonction between two bounds
+	
+	Pre: 'function' is a valid Python expression with x as a variable,
+		'lower' <= 'upper',
+		'function' continuous and integrable between 'lower‘ and 'upper'.
+	Post: Returns an approximation of the integral from 'lower' to 'upper'
+		of the specified 'function'.
+
+	Hint: You can use the 'integrate' function of the module 'scipy' and
+		you'll probably need the 'eval' function to evaluate the function
+		to integrate given as a string.
+	"""
+	pass
+
+if __name__ == '__main__':
+	print(fact(5))
+	print(roots(1, 0, 1))
+	print(integrate('x ** 2 - 1', -1, 1))
